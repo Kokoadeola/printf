330f8f9884d6019e4acf1de09eccf0f98504c437
kokoadeola <kokoadeola84@gmail.com>
2022-10-18 19:20:16 -0700

    2nd push

--- main.h
+++ main.h
@@ -32,4 +32,11 @@ typedef struct fmt fmt_t;
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i,
 		va_list list, char buffer[], int flags, int width, int precision, int size);
+
+/* Funciotns to handle other specifiers */
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
+
 #endif /* MAIN_H */
